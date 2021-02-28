70501bf5444e40c36abba7d5027805d6bc4c1c62
llrantzll <trocchioje@gmail.com>
2021-02-28 18:57:12 -0500

    added custom user model

--- drip_backend/settings.py
+++ drip_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'import_export',
     'rest_framework',
@@ -121,6 +122,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Auth User
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
